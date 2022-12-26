3632a8de162b595c0fad996f89b036cf18c633fa
juzybits <96890594+juzybits@users.noreply.github.com>
2022-12-26 16:44:52 -0100

    [web] doors - wrong/correct banners

--- web/src/js/Doors.tsx
+++ web/src/js/Doors.tsx
@@ -12,34 +12,46 @@ export function Doors(props: any) {
         document.body.className = 'bg-bricks';
     }, []);
 
-    const [modal, setModal] = useState(<></>);
+    const [result, setResult] = useState(null);
 
-    const modalWrong = () => {
-        alert('Wrong!');
+    const onWrong = () => {
+        setResult(<h1 className='mario red guess-result'>Wrong!</h1>);
+        setTimeout(() => {
+            setResult(null);
+        }, 1000);
     }
 
-    const modalCorrect = () => {
-        alert('Correct!');
+    const onCorrect = () => {
+        setResult(<h1 className='mario green guess-result'>Correct!</h1>);
+        setTimeout(() => {
+            setResult(null);
+            props.nextStage();
+        }, 1000);
     }
+
+    const DoorsGrid = (props: any) => {
+        return <div className='door-grid'>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosedDiff} onClick={onCorrect} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={onWrong} alt='closed door' /></div>
+        </div>
+    };
 
     return <div id='page' className='doors'>
-        <div className='door-grid'>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosedDiff} onClick={modalCorrect} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-            <div className='door-wrap'><img className='hand' src={imgDoorClosed} onClick={modalWrong} alt='closed door' /></div>
-        </div>
+        { result && result }
+        <DoorsGrid />
     </div>;
 }
