883a55913d6c968be3241c3ec5f47d436e53497e
juzybits <96890594+juzybits@users.noreply.github.com>
2023-01-26 06:32:06 -0100

    [web] fade-in home elements

--- web/src/js/0_Home.tsx
+++ web/src/js/0_Home.tsx
@@ -9,11 +9,11 @@ export function Home(props: any) {
     }, []);
 
     return <div id='page' className='home'>
-        <h1 className='mario home-title'><span className='prefix'>Journey to</span>Mount Sogol</h1>
-        <p className='home-description paragraph'>
+        <h1 className='mario home-title fade-in-1'><span className='prefix'>Journey to</span>Mount Sogol</h1>
+        <p className='home-description paragraph fade-in-2'>
             Travelers beware: The Journey to Mount Sogol is a peculiar one, because the destination is not a physical place. That is not to say it is not <i>real</i>. However, Mount Sogol is to be found on a different plane of existence, not in the mundane world. But how can it be reached? Well...<br/><br/><i>the door to the invisible must be visible</i>.
         </p>
-        <button className='btn' onClick={props.nextStage}>
+        <button className='btn fade-in-3' onClick={props.nextStage}>
             BEGIN
         </button>
     </div>;
